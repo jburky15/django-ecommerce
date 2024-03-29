4c72869b66885e8e4a0093a0232faf5313b687d5
Joseph P Burkhart <jburkhart@pineenvironmental.com>
2024-03-29 16:05:29 -0400

    Started to add qty updates to cart

--- cart/views.py
+++ cart/views.py
@@ -36,4 +36,11 @@ def cart_delete(request):
     pass
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # Get the product
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        # Update the cart
+        cart.update(product=product_id, quantity=product_quantity)
